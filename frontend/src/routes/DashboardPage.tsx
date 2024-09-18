518744ca1a58348b46fb31235a9ec016386dc126
Tram Vu <82486188+ngoctramvu@users.noreply.github.com>
2024-09-17 20:23:29 -0500

    set up frontend page structures

---
+++ frontend/src/routes/DashboardPage.tsx
@@ -0,0 +1,9 @@
+import HeaderBar from "../components/HeaderBar";
+
+export default function ProfilePage() {
+  return (
+    <div className="dashboard-page">
+      <HeaderBar />
+    </div>
+  )
+}
