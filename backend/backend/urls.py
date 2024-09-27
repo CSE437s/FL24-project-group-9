26e184fce3fb2f8f607d42bec6c3d9cb4da43976
Duy Huynh <duy.huynhngoc.05@gmail.com>
2024-09-27 12:00:21 -0500

    Implement BFF API and Set up Poetry (#25)
    * Remove __pycache__ dir from tracking
    
    * Fix bug
    
    * Update . gitignore
    
    * Add Poetry, and example endpoints
    
    * add scraping script
    
    * implement simple api

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-``"""
+"""
 URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
 ]
