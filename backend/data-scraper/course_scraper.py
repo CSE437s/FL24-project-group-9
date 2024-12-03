8f79fd0e8fc44a14dbacacdf3881f0608645bf3c
SamEGil00 <sam.e.gil@wustl.edu>
2024-12-03 14:17:02 -0600

    Removed comment tags

--- backend/data-scraper/course_scraper.py
+++ backend/data-scraper/course_scraper.py
@@ -52,7 +52,7 @@ class CourseScraper:
 
             soup = BeautifulSoup(page.text, "html.parser")
            
-            #dept = url.split("/", 7)[5]
+            
             department_title = soup.find("h1", class_="page-title")
             dept = department_title.text.strip()
             # Find the course listings in the HTML structure
@@ -90,14 +90,12 @@ class CourseScraper:
                     course_attrib = attrib_dept_tag + attrib_tag
                     course_attrib = course_attrib.split("/search/", 1)[1].strip()
                     course_info["attributes"] = course_attrib.strip().replace("\u00a0", " ")
-                    
+                
                 transformed_course_info = transform_data(course_info)
                 courses_list.append(transformed_course_info)
         
         for c in courses_list:
-            if c in courses_set:
-                pass
-            else:
+            if c not in courses_set:
                 courses_set.append(c)
         
         return courses_set
@@ -106,86 +104,53 @@ class CourseScraper:
 
 page_links = [
     "https://bulletin.wustl.edu/undergrad/engineering/computerscience/#courses",
-#    "https://bulletin.wustl.edu/undergrad/engineering/energy-environmental-chemical/#courses",
-#    "https://bulletin.wustl.edu/undergrad/engineering/biomedical/#courses",
-#    "https://bulletin.wustl.edu/undergrad/engineering/electrical-and-systems/#courses",
-#    "https://bulletin.wustl.edu/undergrad/engineering/umsl-wustl-joint-program/#courses",
-#    "https://bulletin.wustl.edu/undergrad/architecture/#courses",
-#    "https://bulletin.wustl.edu/undergrad/art/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/africanandafricanamericanstudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/americanculturestudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/classics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/anthropology/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/appliedlinguistics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/mathematics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/economics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/jimes/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/arthistoryandarchaeology/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/americanculturestudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/physics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/biology/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/chemistry/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/english/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/eastasianlanguagesandcultures/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/psychologicalandbrainsciences/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/comparativeliteratureandthought/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/performingarts/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/statisticsanddatascience/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/eeps/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/education/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/environmentalstudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/politicalscience/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/filmandmediastudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/romancelanguagesandliteratures/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/globalstudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/history/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/music/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/latinamericanstudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/linguistics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/philosophy/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/philosophyneurosciencepsychology/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/publichealthandsociety/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/religionandpolitics/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/religiousstudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/russianlanguageandliterature/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/sociology/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/speechandhearing/#courses",
-#    "https://bulletin.wustl.edu/undergrad/artsci/womengenderandsexualitystudies/#courses",
-#    "https://bulletin.wustl.edu/undergrad/business/#courses",
-#    "https://bulletin.wustl.edu/undergrad/beyondboundaries/#courses",
-#    "https://bulletin.wustl.edu/grad/art/#courses",
-#    "https://bulletin.wustl.edu/grad/architecture/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/dbbs/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/americanculturestudies/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/anthropology/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/arthistoryandarchaeology/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/biology/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/chemistry/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/classics/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/comparativeliteratureandthought/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/performingarts/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/eeps/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/eastasianlanguagesandcultures/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/economics/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/education/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/english/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/filmandmediastudies/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/romancelanguagesandliteratures/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/history/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/jimes/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/latinamericanstudies/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/liberalarts/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/mathematics/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/music/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/performingarts/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/philosophy/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/philosophyneurosciencepsychology/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/physics/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/politicalscience/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/psychologicalandbrainsciences/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/sociology/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/statisticsanddatascience/#courses",
-#    "https://bulletin.wustl.edu/grad/artsci/womengenderandsexualitystudies/#courses"
+    "https://bulletin.wustl.edu/undergrad/engineering/energy-environmental-chemical/#courses",
+    "https://bulletin.wustl.edu/undergrad/engineering/biomedical/#courses",
+    "https://bulletin.wustl.edu/undergrad/engineering/electrical-and-systems/#courses",
+    "https://bulletin.wustl.edu/undergrad/engineering/umsl-wustl-joint-program/#courses",
+    "https://bulletin.wustl.edu/undergrad/architecture/#courses",
+    "https://bulletin.wustl.edu/undergrad/art/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/africanandafricanamericanstudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/americanculturestudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/classics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/anthropology/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/appliedlinguistics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/mathematics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/economics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/jimes/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/arthistoryandarchaeology/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/americanculturestudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/physics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/biology/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/chemistry/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/english/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/eastasianlanguagesandcultures/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/psychologicalandbrainsciences/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/comparativeliteratureandthought/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/performingarts/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/statisticsanddatascience/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/eeps/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/education/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/environmentalstudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/politicalscience/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/filmandmediastudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/romancelanguagesandliteratures/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/globalstudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/history/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/music/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/latinamericanstudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/linguistics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/philosophy/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/philosophyneurosciencepsychology/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/publichealthandsociety/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/religionandpolitics/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/religiousstudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/russianlanguageandliterature/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/sociology/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/speechandhearing/#courses",
+    "https://bulletin.wustl.edu/undergrad/artsci/womengenderandsexualitystudies/#courses",
+    "https://bulletin.wustl.edu/undergrad/business/#courses",
+    "https://bulletin.wustl.edu/undergrad/beyondboundaries/#courses"
     ]
 
 scraper = CourseScraper(page_links)
